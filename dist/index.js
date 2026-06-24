342f9b1b1095ba918ca06f33dff89a996e3aaf0e
Lê Văn Giàu <levangiau20032020@gmail.com>
2026-06-24 05:27:36 +0000

    chore :fix kí tự

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.writeHead(200, { "Content-Type": "text/plain ; charset=utf-8" });
   res.end("Hello  Giàu đẹp trai nhất thế giới ...................AAAAAAAAAAAAAAAAAAAAA");
 });
 
