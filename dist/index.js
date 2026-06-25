dff0f7a363176350c0047e84f5484fd9c7930106
Lê Văn Giàu <levangiau20032020@gmail.com>
2026-06-25 13:22:20 +0000

    chore: change test to test cicd

--- dist/index.js
+++ dist/index.js
@@ -2,8 +2,8 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain ; charset=utf-8" });
-  res.end("Hello  Giàu đẹp trai nhất thế giới  nha mấy EMMMMMMMMM... babe aaaa !!!!");
-});
+  res.end("Hello mấy em be tới với trang web của anh nha !!!!");
+})
 
 server.listen(3000, "0.0.0.0", () => {
   console.log("Server running on port 3000");
