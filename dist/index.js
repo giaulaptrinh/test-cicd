0c2d676c0a2c6cfc9e0c106c8966fd9f984164bb
Lê Văn Giàu <levangiau20032020@gmail.com>
2026-07-05 05:30:08 +0000

    chore: text jenkins cicd

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain ; charset=utf-8" });
   res.end(
-    "Hello mấy em be tới với trang web sử dụng Jenkins CICD của anh nha !!!!",
+    "Hello mấy em be tới với trang web sử dụng Jenkins CICD của anh nha aaaaaaaa !!!!",
   );
 });
 
