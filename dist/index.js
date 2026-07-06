0fec43e275a5ca29d6f8db213d3f3840a8a253f4
Lê Văn Giàu <levangiau20032020@gmail.com>
2026-07-06 08:13:12 +0000

    chore: change text to CICD-2

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain ; charset=utf-8" });
-  res.end("Hello Ly!!!!");
+  res.end("Hello Giàu đẹp trai nhất server hêheeeeeeeeeeeeeee!!!!");
 });
 
 server.listen(3000, "0.0.0.0", () => {
