32ffce5e212e0277052aa528fb92a5dba1a122a2
Giau Lee <levangiau20032020@gmail.com>
2026-07-05 20:47:07 -0700

    chore :change text to test jenkins cicd

--- dist/index.js
+++ dist/index.js
@@ -2,9 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain ; charset=utf-8" });
-  res.end(
-    "Hello mấy em be tới với trang web sử dụng Jenkins CICD của anh nha aaaaaaaa !!!!",
-  );
+  res.end("Hello Ly!!!!");
 });
 
 server.listen(3000, "0.0.0.0", () => {
